9fc957039185f9ac1dcc1be794f8952111c9b5cd
YehCF <r02429007@ntu.edu.tw>
2018-02-28 23:37:43 +0800

    revision for PEP8 compliance

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,9 +2,11 @@ import json
 import media
 import fresh_tomatoes
 
+
 def extract_movie_trailers_from_json(movies_josn_file):
     '''
-    Extract info about movie trailers from a json file (movie_trailers.json in this case)
+    Extract info about movie trailers from a json file
+    (movie_trailers.json in this case)
     :param movies_josn_file: the path to open the json file (.json)
     :return:
         a list of movie trailers, which are dictionaires
@@ -27,10 +29,11 @@ def extract_movie_trailers_from_json(movies_josn_file):
 
     return movie_list
 
+
 def create_movies(movie_list):
     '''
     Create a list of movie objects from a list of movie dictionaries
-    :param movie_list: a list of movie dictionaries, containing the info about movies
+    :param movie_list: a list of movie dictionaries
         including title, storyline, poster_image_url and youtube_trailer_url
     :return:
         a list of Movie objects
@@ -40,7 +43,8 @@ def create_movies(movie_list):
         movie_object = media.Movie(title=movie['title'],
                                    storyline=movie['storyline'],
                                    poster_image_url=movie['poster_image_url'],
-                                   trailer_youtube_url=movie['trailer_youtube_url'])
+                                   trailer_youtube_url=movie['trailer_'
+                                                             'youtube_url'])
         movie_objects.append(movie_object)
 
     return movie_objects
--- media.py
+++ media.py
@@ -1,11 +1,11 @@
-
 class Movie(object):
     '''
     A class to hold the information about a movie,
     including the title, storyline, poster image and its trailer
     '''
 
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, storyline, poster_image_url,
+                 trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
